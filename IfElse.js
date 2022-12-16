0c1a19c7833a7ce88191e36ffc06ac3baa933497
matheus <matheusbartolo.157.155@gmail.com>
2022-12-15 21:22:16 -0300

    atualizacaojs2

---
+++ IfElse.js
@@ -0,0 +1,20 @@
+let vendas = false;
+if (vendas) {
+    console.log('Hora de comprar');
+} else {
+    console.log('Hora de esperar pela venda');
+}
+
+//Comparações
+let nivelDeFome = 9;
+if (nivelDeFome >= 9) {
+   console.log('Coma agora!')
+} else {
+   console.log('Espera chegar em casa')
+}
+
+// Igualdade == ou ===
+// Diferença != ou !==
+console.log('maca' === 'laranja')
+console.log(8 === 8)
+console.log(8 != 8)
