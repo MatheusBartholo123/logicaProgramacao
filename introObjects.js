01fa1a615f2f8d7b04f0750e15d57df10a403692
matheus <matheusbartolo.157.155@gmail.com>
2023-01-16 21:28:41 -0300

    tarefa atualizada

---
+++ introObjects.js
@@ -0,0 +1,15 @@
+let spaceship = {
+    'Fuel Type' : 'diesel',
+    color: 'silver'
+}
+
+console.log (spaceship);
+
+
+let novaEspaconave = {
+    'Fuel Type' : 'Luz',
+    color: 'azul'
+}
+
+console.log (novaspaconave);
+
