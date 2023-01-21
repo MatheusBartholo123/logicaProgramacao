b55fd94db24f8fd4c2e01a273783d62259edf9cd
matheus <matheusbartolo.157.155@gmail.com>
2023-01-20 21:18:18 -0300

    tarefa8

---
+++ Tarefa8.js
@@ -0,0 +1,41 @@
+// //1 //2 //3 //4 //5 //6 //7 //8
+// const menu  = {
+//   _meal : '',
+//   _price : 0,
+//    get meal () {
+//     return this._meal
+//    },
+//    get price () {
+//      return this._price
+//    },
+//    set meal (mealToCheck) {
+//     if (typeof mealToCheck === 'string') {
+//       return this._meal = mealToCheck;
+//     }
+//    },
+//    set price (priceToCheck) {
+//     if (typeof priceToCheck === 'number') {
+//        return this._price = priceToCheck;
+//     }
+//    },
+//    get todaysSpecial () {
+//      if () {
+
+//      } else {
+
+//      }
+//    }
+// }
+
+
+// menu._meal = 19;
+// console.log (menu.meal)
+
+//menu._price = 'Macarrão';
+//console.log (menu.price)
+
+//console.log (menu);
+
+
+
+
