8f2aaeee34e59af05ffb52dc40f88133f2c5af77
matheus <matheusbartolo.157.155@gmail.com>
2023-01-20 21:35:41 -0300

    tarefa8 atualizada

--- Tarefa8.js
+++ Tarefa8.js
@@ -1,40 +1,48 @@
 // //1 //2 //3 //4 //5 //6 //7 //8
-// const menu  = {
-//   _meal : '',
-//   _price : 0,
-//    get meal () {
-//     return this._meal
-//    },
-//    get price () {
-//      return this._price
-//    },
-//    set meal (mealToCheck) {
-//     if (typeof mealToCheck === 'string') {
-//       return this._meal = mealToCheck;
-//     }
-//    },
-//    set price (priceToCheck) {
-//     if (typeof priceToCheck === 'number') {
-//        return this._price = priceToCheck;
-//     }
-//    },
-//    get todaysSpecial () {
-//      if () {
+const menu  = {
+  _meal : '',
+  _price : 0,
+   get meal () {
+    return this._meal
+   },
+   get price () {
+     return this._price
+   },
+   get todaysSpecial () {
+      if (this._meal && this._price) {
+         return `O especial de hoje é ${this._meal} por R$ ${this._price} `
 
-//      } else {
+      } else {
+          return 'Refeição ou preço não foi definido corretamente!';
+      }
+   },
 
-//      }
-//    }
-// }
+   set meal (mealToCheck) {
+    if (typeof mealToCheck === 'string') {
+      return this._meal = mealToCheck;
+    }
+   },
+   set price (priceToCheck) {
+    if (typeof priceToCheck === 'number') {
+       return this._price = priceToCheck;
+    }
+   }
+}
 
 
-// menu._meal = 19;
-// console.log (menu.meal)
+menu.price = 23;
+menu.meal = 'pizza';
+
+//menu._meal = 19;
+//console.log (menu.meal)
 
 //menu._price = 'Macarrão';
 //console.log (menu.price)
 
-//console.log (menu);
+console.log (menu.meal);
+console.log (menu.price);
+console.log (menu.todaysSpecial);
+console.log (menu);
 
 
 
